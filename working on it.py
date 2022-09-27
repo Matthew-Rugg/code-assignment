83575b6743a837aa162668ef18a7de3d5c1f8d78
Matthew-Rugg <114540021+Matthew-Rugg@users.noreply.github.com>
2022-09-27 19:50:19 -0400

    Add files via upload

---
+++ working on it.py
@@ -0,0 +1,39 @@
+keep_going = 'y'
+#Validate it
+#if good - proceed
+#if not - don't proceed, ask user if they want to do it again, if yes, repeat process
+#Get user input
+        
+while keep_going == 'y':
+        Num1 = (input('Enter your first number: '))
+        if (Num1.isnumeric) == False:
+                Num1 = (input('Please enter a number this time: '))
+        else:
+                while keep_going == 'y':
+                        Num2 = (input('Enter your second number: '))
+                        if (Num2.isnumeric) == False:
+                                Num2= (input('Please enter a number this time: '))
+                        else:
+                                        while keep_going == 'y':
+                                                Op = input('Enter your operation (+,-,/,*,%): ')
+                                                if Op == '+':
+                                                    print(int(Num1) + int(Num2))
+                                                    keep_going = input('Do you need another calculation? (Enter y for yes): ')
+                                                elif Op == '-':
+                                                    print(int(Num1) - int(Num2))
+                                                    keep_going = input('Do you need another calculation? (Enter y for yes): ')
+                                                elif Op == '/':
+                                                    print(int(Num1) / int(Num2))
+                                                    keep_going = input('Do you need another calculation? (Enter y for yes): ')
+                                                elif Op == '*':
+                                                    print(int(Num1) * int(Num2))
+                                                    keep_going = input('Do you need another calculation? (Enter y for yes): ')
+                                                elif Op == '%':
+                                                    print((int(Num1))/100 * int(Num2))
+                                                    keep_going = input('Do you need another calculation? (Enter y for yes): ')        
+                                                else:
+                                                    print('Please enter a correct operation(+,-,/,*,%): ')
+
+
+
+
